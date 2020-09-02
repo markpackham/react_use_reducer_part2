f7a0f88674614e97406643e5dcfb33867c810077
mark <markpackham1@gmail.com>
2020-09-02 11:47:42 +0100

    output todo in console

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ const ACTIONS = {
   ADD_TODO: "add-todo",
 };
 
-function reducer(todos, action) {}
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, []);
+  const [todos, dispatch] = useReducer(reducer, []);
   const [name, setName] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    dispatch({ type: ACTIONS.ADD_TODO });
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
     setName("");
   }
+
+  console.log(todos);
 
   return (
     <>
